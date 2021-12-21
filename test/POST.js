71599d3cbe221d5a8122322492f557246c0e6a1e
Gal <galsened@gmail.com>
2021-12-21 15:33:42 +0200

    ok

--- test/POST.js
+++ test/POST.js
@@ -1,8 +1,9 @@
 import { describe } from "mocha";
 import supertest from "supertest";
 const request = supertest('https://gorest.co.in/public-api/')
-
+const faker = require ('faker');
 import { expect } from "chai";
+import { name } from "faker";
 
 const TOKEN = 
     '4ef990d9537c541aef058b3a4421fd6d3fcdfcf881314fc13af0535ff1304158'
@@ -13,18 +14,16 @@ describe('POST',()=>{
     let UserId;
     it('post1', () => {
         const data ={
-            email: `test${Math.floor(Math.random() * 9999 )}@gmail.com`,
-            name: 'lory',
-            gender: 'male',
+            email: faker.internet.email(),
+            name: faker.name.firstName(),
+            gender: faker.name.gender(),
             status: 'inactive'
         }
-
+console.log(data);
         return request.post('users').set("Authorization",`Bearer ${TOKEN}`).send(data).then((res)=>{
             
-            
-            console.log(data.email);
             console.log(res.body.data.email);
-            expect(res.body.data).deep.include(data);
+            expect(res.body.data).include(data);
             UserId=res.body.data.id;
             console.log(UserId);
         });
--- test/PUT.js
+++ test/PUT.js
@@ -1,7 +1,7 @@
 import { describe } from "mocha";
 import supertest from "supertest";
 const request = supertest('https://gorest.co.in/public-api/')
-
+const faker = require ('faker');
 import { expect } from "chai";
 
 const TOKEN = 
@@ -16,13 +16,15 @@ describe('PUT',()=>{
         }
 
         return request
-            .put('users/132')
+            .put('users/1156')
             .set('authorization', `Bearer ${TOKEN}`)
             .send(data)
-            .then(res=>{
+            .then((res=>{
+                console.log(res.body);
                 expect(res.body.data.name).to.be.equal(data.name)
-            })
+            }));
 
+    
 
     });
 
